d1cf49c883dc37b84f8c4dedbaffa7563fc950ab
Eunji Nam <godangel31@gmail.com>
2021-11-11 11:26:55 +0900

    초간단 사이트 구축

--- app.py
+++ app.py
@@ -1,7 +1,28 @@
-from flask import Flask
+# pip install flask
+
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
-    return "Hello, Heroku"
+    return render_template("index.html")
+
+
+@app.route("/profile")
+def profile():
+    # DB Access
+    like_foods = [
+        "초밥",
+        "육회",
+        "샤브샤브",
+        "쌀국수",
+        "대창",
+    ]
+    return render_template("profile.html", like_foods=like_foods)
+
+
+@app.route("/posts")
+def post_list():  # ->누가 받을지 설정
+    return render_template("post_list.html")
